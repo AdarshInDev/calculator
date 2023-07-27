c6243947edf53a67a76d0151e010f27d96e2cf70
Adarsh Pradhan <dev.apradhan1508@gmail.com>
2023-07-27 23:11:11 +0530

    initial commmit

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    const MaterialApp(
+   
+    ),
+  );
+}
+class Homescreen extends StatefulWidget {
+  const Homescreen({super.key});
+
+  @override
+  State<Homescreen> createState() => _HomescreenState();
+}
+
+class _HomescreenState extends State<Homescreen> {
+  @override
+  Widget build( context) {
+    return const Placeholder();
+  }
+}
